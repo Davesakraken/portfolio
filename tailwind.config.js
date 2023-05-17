c954a120d07d310a5fd6c768aaabc0ce6cfab8b9
Davesakraken <davidgeorgebell1995@outlook.com>
2023-05-17 15:12:44 +0100

    deployment-fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}", "./src/app/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx},",
+    "'./src/**/*.{js,ts,jsx,tsx}'",
+  ],
   theme: {
     extend: {
       backgroundImage: {
