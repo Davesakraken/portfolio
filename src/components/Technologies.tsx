700572a32c1ac19e9ad1a60f03776a9aea1641d9
Davesakraken <davidgeorgebell1995@outlook.com>
2023-05-22 14:18:51 +0100

    Icon animations

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -5,33 +5,33 @@ export default function MyTechnologies() {
     <section className="text-center">
       <div>
         <h2>Technologies I use ✨</h2>
-        <div className="mt-20 text-6xl flex justify-center flex-wrap sm:gap-7 md:gap-15">
+        <div className=" mt-20 text-6xl flex justify-center flex-wrap sm:gap-7 md:gap-15">
           <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank">
-            <SiHtml5 className=" text-orange-600 m-5 cursor-pointer" />
+            <SiHtml5 className="transition-all-02 transform-scale-hover text-orange-600 m-5 cursor-pointer" />
           </a>
 
           <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank">
-            <SiCss3 className="text-blue-700 m-5" />
+            <SiCss3 className="transition-all-02 transform-scale-hover text-blue-700 m-5" />
           </a>
 
           <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-            <SiJavascript className="text-yellow-400 m-5" />
+            <SiJavascript className="transition-all-02 transform-scale-hover text-yellow-400 m-5" />
           </a>
 
           <a href="https://www.typescriptlang.org/" target="_blank">
-            <SiTypescript className="text-blue-700 m-5" />
+            <SiTypescript className="transition-all-02 transform-scale-hover text-blue-700 m-5" />
           </a>
 
           <a href="https://react.dev/" target="_blank">
-            <SiReact className="text-blue-400 m-5" />
+            <SiReact className="transition-all-02 transform-scale-hover text-blue-400 m-5" />
           </a>
 
           <a href="https://nextjs.org/" target="_blank">
-            <SiNextdotjs className="text-slate-800-400 m-5" />
+            <SiNextdotjs className="transition-all-02 transform-scale-hover text-slate-800-400 m-5" />
           </a>
 
           <a href="https://tailwindcss.com/" target="_blank">
-            <SiTailwindcss className="text-blue-400 m-5" />
+            <SiTailwindcss className="transition-all-02 transform-scale-hover text-blue-400 m-5" />
           </a>
         </div>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 // Components
 import Header from "../components/Header";
-import MyTechnologies from "../components/MyTechnologies";
+import MyTechnologies from "../components/Technologies";
 import Background from "../components/Background";
 import Card from "../components/CardComponents/Card";
 import { CardData } from "../components/CardComponents/CardData";
@@ -16,7 +16,7 @@ export default function Home() {
         <Header />
         <MyTechnologies />
         <section>
-          <h2 className="pt-40">My Projects 🚀</h2>
+          <h2 className=" pt-40">My Projects 🚀</h2>
           <div className="flex flex-row flex-wrap justify-center mt-20 gap-5 p-10">
             {CardData.map((card) => (
               <Card
