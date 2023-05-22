d73be45cdd129982674ce525ff0c65e51d1e9cd0
Davesakraken <davidgeorgebell1995@outlook.com>
2023-05-22 01:04:40 +0100

    Technology links

--- src/components/MyTechnologies.tsx
+++ src/components/MyTechnologies.tsx
@@ -1,4 +1,4 @@
-import { SiCss3, SiHtml5, SiJavascript, SiReact, SiTailwindcss } from "react-icons/si";
+import { SiCss3, SiHtml5, SiJavascript, SiReact, SiTailwindcss, SiNextdotjs, SiTypescript } from "react-icons/si";
 
 export default function MyTechnologies() {
   return (
@@ -6,11 +6,33 @@ export default function MyTechnologies() {
       <div>
         <h2>Technologies I use ✨</h2>
         <div className="mt-20 text-6xl flex justify-center flex-wrap sm:gap-7 md:gap-15">
-          <SiHtml5 className=" text-orange-600 m-5" />
-          <SiCss3 className="text-blue-700 m-5" />
-          <SiJavascript className="text-yellow-400 m-5" />
-          <SiReact className="text-blue-400 m-5" />
-          <SiTailwindcss className="text-blue-400 m-5" />
+          <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank">
+            <SiHtml5 className=" text-orange-600 m-5 cursor-pointer" />
+          </a>
+
+          <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank">
+            <SiCss3 className="text-blue-700 m-5" />
+          </a>
+
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+            <SiJavascript className="text-yellow-400 m-5" />
+          </a>
+
+          <a href="https://www.typescriptlang.org/" target="_blank">
+            <SiTypescript className="text-blue-700 m-5" />
+          </a>
+
+          <a href="https://react.dev/" target="_blank">
+            <SiReact className="text-blue-400 m-5" />
+          </a>
+
+          <a href="https://nextjs.org/" target="_blank">
+            <SiNextdotjs className="text-slate-800-400 m-5" />
+          </a>
+
+          <a href="https://tailwindcss.com/" target="_blank">
+            <SiTailwindcss className="text-blue-400 m-5" />
+          </a>
         </div>
       </div>
     </section>
