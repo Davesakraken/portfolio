6e7b709b02c862668680995d0790988d6ea608af
Davesakraken <davidgeorgebell1995@outlook.com>
2023-07-03 19:50:38 +0100

    padding

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,7 +8,7 @@ export default function Heading() {
     <header className="grid grid-cols-1 grid-rows-1">
       <div className="min-h-screen flex flex-col justify-start md:justify-center">
         <BurgerMenu />
-        <div className="mt-60 md:mt-20 text-center select-none ">
+        <div className="mt-60 md:mt-24 text-center select-none ">
           <h1 className="text-7xl md:text-9xl fade-in-top header-gradient bg-clip-text text-transparent">David Bell</h1>
           <HeadingChanger />
           <Navbar />
