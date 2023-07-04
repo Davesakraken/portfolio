d32735dcc7fa337dd6d7cf3dc782beee9966ca24
Davesakraken <davidgeorgebell1995@outlook.com>
2023-07-04 20:48:23 +0100

    a tag rel fix

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -25,6 +25,7 @@ export default function Card({ title, desc, btnText, btnHref, imgSrc }: Props) {
           <a
             href={btnHref}
             target="_blank"
+            rel="noreferrer"
             className="cursor-pointer inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg
              hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
--- src/components/NavBar/IconMenu.tsx
+++ src/components/NavBar/IconMenu.tsx
@@ -6,17 +6,17 @@ export default function IconMenu() {
     <nav className="hidden mt-12 md:block">
       <ul className="flex justify-center text-4xl gap-16 text-slate-600">
         <li>
-          <a href="https://www.linkedin.com/in/david-bell-39bb03265/" target="_blank">
+          <a href="https://www.linkedin.com/in/david-bell-39bb03265/" target="_blank" rel="noreferrer">
             <BsLinkedin className="cursor-pointer" />
           </a>
         </li>
         <li>
-          <a href="https://github.com/Davesakraken" target="_blank">
+          <a href="https://github.com/Davesakraken" target="_blank" rel="noreferrer">
             <BsGithub className="cursor-pointer" />
           </a>
         </li>
         <li>
-          <a href="https://twitch.tv/davesakraken" target="_blank">
+          <a href="https://twitch.tv/davesakraken" target="_blank" rel="noreferrer">
             <BsTwitch className="cursor-pointer" />
           </a>
         </li>
