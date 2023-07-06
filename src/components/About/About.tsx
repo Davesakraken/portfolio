f9282d852440d3c12d85411727c112037050a194
Davesakraken <davidgeorgebell1995@outlook.com>
2023-07-06 20:36:17 +0100

    sizing fix

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,9 +3,9 @@ import styles from "./about.module.css";
 export default function MyTechnologies() {
   return (
     <section id="about">
-      <div className="flex justify-center gap-8 flex-wrap header-gradient">
+      <div className="flex justify-center gap-8 flex-wrap header-gradient align-middle">
         <div className={styles.aboutBox}>
-          <p className="absolute top-10 p-8 align-middle text-center text-2xl ">
+          <p className="m-5 mt-10 align-middle text-center text-2xl">
             A passionate developer specializing in frontend web development. I bring designs to life with precision and creativity to
             deliver seamless user experiences.
           </p>
